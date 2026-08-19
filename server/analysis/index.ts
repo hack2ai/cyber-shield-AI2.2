635660dbd913dd1c39ee9684bd47e1db2c75b5b0
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-08-19 12:23:17 +0530

    feat: expose public analysis engine API

---
+++ server/analysis/index.ts
@@ -0,0 +1,6 @@
+export { analyzeUrl } from './analyzer.js';
+export { extractUrlFeatures, findingsFromUrlFeatures } from './features.js';
+export { calculateRisk } from './scoring.js';
+export type { UrlAnalysisResult } from './types.js';
+export type { RiskAssessment, RiskLevel, SecurityFinding } from './scoring.js';
+export type { UrlFeatures } from './features.js';
