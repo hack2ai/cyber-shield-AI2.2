cad11d9e93cf766ca2a3520b08c81b6b2a7ac163
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-08-29 14:04:15 +0530

    feat: export request correlation middleware

--- server/security/index.ts
+++ server/security/index.ts
@@ -1,3 +1,4 @@
 export { rateLimit } from './rate-limit.js';
 export { securityHeaders } from './security-headers.js';
 export { isBlockedIp, validateExternalUrl } from './url-validator.js';
+export { requestContext, REQUEST_ID_HEADER } from './request-context.js';
